9b07929f6de7e09094d05f1e512a5cfb50cc20e5
antonturov <antonturov@mail.ru>
2021-10-04 16:47:34 +0300

    Добавлены переменные totalWeight и limitWeight, выставлен лимит веса. Добавлен объект Basket, отражающий вес продуктов в корзине. Так же в метод add добавлено значение веса. Написан метод getTotalWeight и все выведено на консоль с расчетом веса и стоимости.

--- 03_ObjectsAndMethods/src/Basket.java
+++ 03_ObjectsAndMethods/src/Basket.java
@@ -3,12 +3,15 @@ public class Basket {
     private static int count = 0;
     private String items = "";
     private int totalPrice = 0;
+    private double totalWeight = 0;
     private int limit;
+    private double limitWeight;
 
     public Basket() {
         increaseCount(1);
         items = "Список товаров:";
         this.limit = 1000000;
+        this.limitWeight = 3000;
     }
 
     public Basket(int limit) {
@@ -21,6 +24,12 @@ public class Basket {
         this.items = this.items + items;
         this.totalPrice = totalPrice;
     }
+    public Basket(String items, int totalPrice, double totalWeight) {
+        this();
+        this.items = this.items + items;
+        this.totalPrice = totalPrice;
+        this.totalWeight = totalWeight;
+    }
 
     public static int getCount() {
         return count;
@@ -30,11 +39,11 @@ public class Basket {
         Basket.count = Basket.count + count;
     }
 
-    public void add(String name, int price) {
-        add(name, price, 1);
+    public void add(String name, int price, double weight) {
+        add(name, price, weight, 1);
     }
 
-    public void add(String name, int price, int count) {
+    public void add(String name, int price, double weight, int count) {
         boolean error = false;
         if (contains(name)) {
             error = true;
@@ -44,14 +53,19 @@ public class Basket {
             error = true;
         }
 
+        //if(totalWeight + weight >= limitWeight){
+        //  error = true;
+        //}
+
         if (error) {
             System.out.println("Error occured :(");
             return;
         }
 
         items = items + "\n" + name + " - " +
-            count + " шт. - " + price;
+                count + " шт. - " + price + "(" + weight + ")";
         totalPrice = totalPrice + count * price;
+        totalWeight = totalWeight + count * weight;
     }
 
     public void clear() {
@@ -67,12 +81,17 @@ public class Basket {
         return items.contains(name);
     }
 
+    public double getTotalWeight() {
+        return totalWeight;
+    }
+
     public void print(String title) {
         System.out.println(title);
         if (items.isEmpty()) {
             System.out.println("Корзина пуста");
         } else {
             System.out.println(items);
+            System.out.println("Итоговая цена:" + totalPrice + " Итоговый вес:" + totalWeight);
         }
     }
 }
--- 03_ObjectsAndMethods/src/Main.java
+++ 03_ObjectsAndMethods/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Basket basket = new Basket();
-        basket.add("Milk", 40);
+        basket.add("Milk", 40, 400, 5);
+        basket.add("Cheese", 80, 600, 5);
+        basket.add("Bread", 410, 700, 2);
         basket.print("Milk");
     }
 }
