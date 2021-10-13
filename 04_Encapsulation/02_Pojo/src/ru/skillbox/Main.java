d0530377f6e256ac3f97dc3c214d6e3b8fa9954f
antonturov <antonturov@mail.ru>
2021-10-13 20:05:31 +0300

    Pojo add

--- 04_Encapsulation/02_Pojo/src/ru/skillbox/Main.java
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Main.java
@@ -1,7 +1,12 @@
 package ru.skillbox;
 
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Country country = new Country ("Russia", 150, 600, "Moscow", true);
+        System.out.println(country.getCountryName());
+        System.out.println(country.getCapitalOfCountry());
+        System.out.println(country.getSquareCountry());
+        System.out.println(country.getPopulationOfCountry());
+        System.out.println(country.hasSeaGate());
     }
 }
