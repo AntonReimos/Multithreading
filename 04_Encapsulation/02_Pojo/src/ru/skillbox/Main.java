908aa66a1399c0e3ec7f9661b1e9ca669550344c
antonturov <antonturov@mail.ru>
2021-10-13 20:04:20 +0300

    Revert "Pojo add"
    This reverts commit 884e82457082e308035a04613190216c1a3b24ad.

--- 04_Encapsulation/02_Pojo/src/ru/skillbox/Main.java
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Main.java
@@ -1,12 +1,7 @@
 package ru.skillbox;
 
 public class Main {
-    public static void main(String[] args){
-        Country country = new Country ("Russia", 150, 600, "Moscow", true);
-        System.out.println(country.getCountryName());
-        System.out.println(country.getCapitalOfCountry());
-        System.out.println(country.getSquareCountry());
-        System.out.println(country.getPopulationOfCountry());
-        System.out.println(country.hasSeaGate());
+
+    public static void main(String[] args) {
     }
 }
