424b80ac9c8ee0c32f89a63e71431a0ea5615452
antonturov <antonturov@mail.ru>
2021-11-07 23:19:25 +0300

    Add enums, ArithmeticCalculator and method calculate

--- 05_StaticFinal/02_enum/src/ru/skillbox/ArithmeticCalculator.java
+++ 05_StaticFinal/02_enum/src/ru/skillbox/ArithmeticCalculator.java
@@ -2,15 +2,35 @@ package ru.skillbox;
 
 public class ArithmeticCalculator {
 
-    private double a = 0 ;
+    private double a = 0;
     private double b = 0;
+    private double c = 0;
 
-    public void add (double a, double b){
+    public void add(double a, double b) {
         this.a = a;
         this.b = b;
     }
 
-    public void calculate(){
+    public static Operation type;
 
+    public ArithmeticCalculator(Operation type) {
+        this.type = type;
+    }
+
+    public Operation getType() {
+        return type;
+    }
+
+
+    public void calculate() {
+        if (getType() == Operation.ADD) {
+            c = a + b;
+        } else if (getType() == Operation.SUBTRACT) {
+            c = a - b;
+        } else if (getType() == Operation.MULTIPLY) {
+            c = a * b;
+        }
+        System.out.println(c);
     }
 }
+
--- 05_StaticFinal/02_enum/src/ru/skillbox/Main.java
+++ 05_StaticFinal/02_enum/src/ru/skillbox/Main.java
@@ -1,8 +1,11 @@
-﻿package ru.skillbox;
+package ru.skillbox;
 
 public class Main {
 
     public static void main(String[] args) {
-       
+
+        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator(Operation.ADD);
+        arithmeticCalculator.add(7, 8);
+        arithmeticCalculator.calculate();
     }
 }
