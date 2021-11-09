0aa97ca512491f0f81c8ac1285ec10bb05cb6968
antonturov <antonturov@mail.ru>
2021-11-09 18:20:15 +0300

    show the entire Russian alphabet

--- 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -2,17 +2,12 @@ public class Container {
     private Integer count;
 
     public void addCount(int value) {
-<<<<<<< HEAD
+
         count = 0;
-       count = count + value;
-    }
-
-
-=======
         count = count + value;
     }
 
->>>>>>> origin/master
+
     public int getCount() {
         return count;
     }
