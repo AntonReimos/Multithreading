76ecc1b8a947a15886ff8b7616f7f5d9c43073b0
antonturov <antonturov@mail.ru>
2022-04-04 10:10:47 +0300

    done

--- AdvancedOOPFeatures/homework_1/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_1/src/main/java/Main.java
@@ -1,15 +1,41 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    public static final String STAFF_TXT = "data/staff.txt";
+    public static final String STAFF_TXT = "homework_1/data/staff.txt";
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
-        System.out.println(staff);
+        sortBySalaryAndAlphabet(staff);
+
+
     }
+
+    //TODO: В классе Main реализуйте метод sortBySalaryAndAlphabet(),
+    // который должен выполнить сортировку переданного списка сотрудников
+    // по заработной плате и алфавиту. То есть в начале списка будут
+    // минимальные значения зарплат, если значение зарплат одинаковое
+    // у сотрудников, то сотрудники с одинаковой зарплатой должны быть
+    // отсортированы по Ф. И. О.
+    // Проверьте ваш метод с помощью тестов.
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
         //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+
+        List<Employee> sortStaff = staff.stream().sorted(
+                Comparator.comparing(Employee::getSalary).thenComparing(Employee::getName)
+        ).collect(Collectors.toList());
+
+
+        for (Employee employee : sortStaff) {
+            System.out.println(employee);
+        }
+
+
     }
 }
