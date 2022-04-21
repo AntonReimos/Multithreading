a40fd611617acefdc5a0f7393fcb4b43ef87562a
antonturov <antonturov@mail.ru>
2022-04-21 21:12:10 +0300

    Done

--- AdvancedOOPFeatures/homework_1/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_1/src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Main {
 
-    public static final String STAFF_TXT = "homework_1/data/staff.txt";
+    public static final String STAFF_TXT = "data/staff.txt";
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,38 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+import com.skillbox.airport.Terminal;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Airport airport = Airport.getInstance();
+        System.out.println(findPlanesLeavingInTheNextTwoHours(airport));
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+
+        Date dateNow = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dateNow);
+        calendar.add(Calendar.HOUR_OF_DAY, 2);
+        Date datePlus_2 = calendar.getTime();
+
+        List<Flight> outTest = new ArrayList<>();
+
+        for (int i = 0; i < airport.getTerminals().size(); i++) {
+            outTest.addAll(airport.getTerminals().get(i).getFlights());
+        }
+
+        return outTest.stream().filter(flight -> flight.getDate().
+                after(dateNow) && flight.getDate().
+                before(datePlus_2) && flight.getType().
+                equals(Flight.Type.DEPARTURE)).collect(Collectors.toList());
     }
 
 }
