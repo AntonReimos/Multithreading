6c1d84f2dd19300ddd4cb0bd2b798567899a5a54
antonturov <antonturov@mail.ru>
2022-05-20 14:11:24 +0300

    Add marker log

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/java/Main.java
@@ -1,5 +1,9 @@
 import core.Line;
 import core.Station;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -11,13 +15,28 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    private static final String DATA_FILE =  "homework_2\\SPBMetro\\src\\main\\resources\\map.json";//"src/main/resources/map.json";
+    private static final String DATA_FILE = "ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/main/resources/map.json";
+    private static final Marker search = MarkerManager.getMarker("SEARCH"); //добавил
+    private static final Marker errors = MarkerManager.getMarker("ERRORS");//добавил
+    private static final Marker debugs = MarkerManager.getMarker("DEBUG");//добавил
+    private static Logger rootLogger;//добавил
     private static Scanner scanner;
+    private static Logger loggerSearch;
+    private static Logger loggerErrors;
+    private static Logger loggerExceptions;
+
+
 
     private static StationIndex stationIndex;
 
     public static void main(String[] args) {
+        loggerSearch = LogManager.getLogger("SPB_Search_Log");
+        loggerErrors = LogManager.getLogger("SPB_Error_Log");
+        loggerExceptions = LogManager.getLogger("SPB_Debug_Log");
+
+        rootLogger = LogManager.getRootLogger();
         RouteCalculator calculator = getRouteCalculator();
+
 
         System.out.println("Программа расчёта маршрутов метрополитена Санкт-Петербурга\n");
         scanner = new Scanner(System.in);
@@ -38,7 +57,12 @@ public class Main {
     }
 
     private static RouteCalculator getRouteCalculator() {
-        createStationIndex();
+        try {
+            createStationIndex();
+
+        } catch (Exception ex) {
+            //loggerExceptions.debug(ex + " - " + "Не удалось создать stationIndex");
+        }
         return new RouteCalculator(stationIndex);
     }
 
@@ -64,8 +88,12 @@ public class Main {
             String line = scanner.nextLine().trim();
             Station station = stationIndex.getStation(line);
             if (station != null) {
+                //loggerSearch.info("Найдена станция \"" + line + "\"");
+                rootLogger.info(search, "Найдена станция \"" + line + "\" с маркером" );//добавил
                 return station;
             }
+            //loggerErrors.error("Станция \"" + line + "\" не найдена!");
+            rootLogger.error(errors,"Станция \"" + line + "\" не найдена! с маркером" );//добавил
             System.out.println("Станция не найдена :(");
         }
     }
@@ -85,48 +113,66 @@ public class Main {
             JSONArray connectionsArray = (JSONArray) jsonData.get("connections");
             parseConnections(connectionsArray);
         } catch (Exception ex) {
-            ex.printStackTrace();
+            //loggerExceptions.debug(ex + " - " + "Невозможно прочитать файл");
+            rootLogger.debug(debugs, ex + " - " + "Невозможно прочитать файл с маркером");//добавил
         }
     }
 
     private static void parseConnections(JSONArray connectionsArray) {
-        connectionsArray.forEach(connectionObject ->
-        {
-            JSONArray connection = (JSONArray) connectionObject;
-            List<Station> connectionStations = new ArrayList<>();
-            connection.forEach(item ->
+        try {
+            connectionsArray.forEach(connectionObject ->
             {
-                JSONObject itemObject = (JSONObject) item;
-                int lineNumber = ((Long) itemObject.get("line")).intValue();
-                String stationName = (String) itemObject.get("station");
+                JSONArray connection = (JSONArray) connectionObject;
+                List<Station> connectionStations = new ArrayList<>();
 
-                Station station = stationIndex.getStation(stationName, lineNumber);
-                if (station == null) {
-                    throw new IllegalArgumentException("core.Station " +
-                            stationName + " on line " + lineNumber + " not found");
-                }
-                connectionStations.add(station);
+                connection.forEach(item ->
+                {
+                    JSONObject itemObject = (JSONObject) item;
+                    int lineNumber = ((Long) itemObject.get("line")).intValue();
+                    String stationName = (String) itemObject.get("station");
+
+                    Station station = stationIndex.getStation(stationName, lineNumber);
+                    if (station == null) {
+                        throw new IllegalArgumentException("core.Station " +
+                                stationName + " on line " + lineNumber + " not found");
+                    }
+                    connectionStations.add(station);
+
+                });
+                stationIndex.addConnection(connectionStations);
+
             });
-            stationIndex.addConnection(connectionStations);
-        });
+        } catch (Exception ex) {
+            //loggerExceptions.debug(ex + " - " + "Невозмоно прочитать пересадки");
+            rootLogger.debug(debugs,ex + " - " + "Невозмоно прочитать пересадки с маркером" );// добавил
+            ex.getMessage();
+        }
     }
 
     private static void parseStations(JSONObject stationsObject) {
-        stationsObject.keySet().forEach(lineNumberObject ->
-        {
-            int lineNumber = Integer.parseInt((String) lineNumberObject);
-            Line line = stationIndex.getLine(lineNumber);
-            JSONArray stationsArray = (JSONArray) stationsObject.get(lineNumberObject);
-            stationsArray.forEach(stationObject ->
+        try {
+            stationsObject.keySet().forEach(lineNumberObject ->
             {
-                Station station = new Station((String) stationObject, line);
-                stationIndex.addStation(station);
-                line.addStation(station);
+                int lineNumber = Integer.parseInt((String) lineNumberObject);
+                Line line = stationIndex.getLine(lineNumber);
+                JSONArray stationsArray = (JSONArray) stationsObject.get(lineNumberObject);
+                stationsArray.forEach(stationObject ->
+                {
+                    Station station = new Station((String) stationObject, line);
+                    stationIndex.addStation(station);
+                    line.addStation(station);
+                });
             });
-        });
+        } catch (Exception ex) {
+            //loggerExceptions.debug(ex + " - " + "Невозмоно прочитать станции");
+            rootLogger.debug(debugs,ex + " - " + "Невозмоно прочитать пересадки с маркером" );// добавил
+        }
     }
 
     private static void parseLines(JSONArray linesArray) {
+        try {
+
+
         linesArray.forEach(lineObject -> {
             JSONObject lineJsonObject = (JSONObject) lineObject;
             Line line = new Line(
@@ -135,6 +181,10 @@ public class Main {
             );
             stationIndex.addLine(line);
         });
+    }catch (Exception ex){
+            //loggerExceptions.debug(ex + " - " + "Невозмоно прочитать линии");
+            rootLogger.debug(debugs,ex + " - " + "Невозмоно прочитать пересадки с маркером" );// добавил
+        }
     }
 
     private static String getJsonFile() {
@@ -143,7 +193,8 @@ public class Main {
             List<String> lines = Files.readAllLines(Paths.get(DATA_FILE));
             lines.forEach(line -> builder.append(line));
         } catch (Exception ex) {
-            ex.printStackTrace();
+            //loggerExceptions.debug(ex + " - " + "Неправильный путь к файлу");
+            rootLogger.debug(debugs,ex + " - " + "Невозмоно прочитать пересадки с маркером" );// добавил
         }
         return builder.toString();
     }
