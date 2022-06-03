5716f629c787c210d8236bb855e24468afd992e8
antonturov <antonturov@mail.ru>
2022-06-03 17:07:05 +0300

    Size done

--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -3,8 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String path = "C:/users/antonturov/Desktop/java_test";
         System.out.println("Размер папки: " + path + " составляет "
-                + FileUtils.calculateFolderSize(path) + "Byte");
-
+                + FileUtils.calculateFolderSize(path) + " Byte");
 
     }
 
