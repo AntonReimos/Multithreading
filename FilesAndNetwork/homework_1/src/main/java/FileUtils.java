bcb4fca923338aed892a4f9d39cf005896a565f0
antonturov <antonturov@mail.ru>
2022-06-03 13:00:13 +0300

    Size done

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,19 @@
+import java.io.File;
+
 public class FileUtils {
+    private static long size;
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        File folder = new File(path);
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        size = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            size += calculateFolderSize(file.getPath());
+        }
+        return size;
     }
 }
+
--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -1,5 +1,13 @@
+
 public class Main {
     public static void main(String[] args) {
+        String path = "C:/users/antonturov/Desktop/java_test";
+        System.out.println("Размер папки: " + path + " составляет "
+                + FileUtils.calculateFolderSize(path) + "Byte");
+
 
     }
+
+
+
 }
