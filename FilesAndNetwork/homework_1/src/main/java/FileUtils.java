73d2d2acc2ecebf7a5341a57d738e18671df6f57
antonturov <antonturov@mail.ru>
2022-06-03 13:20:07 +0300

    Size done

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -3,19 +3,6 @@ import java.io.File;
 public class FileUtils {
     private static long size;
 
-//    public static long calculateFolderSize(String path) {
-//        File folder = new File(path);
-//        if (folder.isFile()) {
-//            return folder.length();
-//        }
-//        size = 0;
-//        File[] files = folder.listFiles();
-//        for (File file : files) {
-//            size += calculateFolderSize(file.getPath());
-//        }
-//        return size;
-//    }
-
     public static long calculateFolderSize(String path) {
         File folder = new File(path);
         size = 0;
