edd8ea02afdc1d817df607fb36c54dadc032b185
antonturov <antonturov@mail.ru>
2022-06-11 16:23:10 +0300

    the program works with the folder, but the tests fail.

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,5 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
+
+    private static String defSouPath = "";
+    private static String defDestPath = "";
+
+
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
         // TODO: write code copy content of sourceDirectory to destinationDirectory
+        if (defSouPath.isEmpty()) {
+            defSouPath = sourceDirectory;
+            defDestPath = destinationDirectory;
+        }
+        File sourceDir = new File(sourceDirectory);
+        File destDir = new File(destinationDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+            if (sourceDir.isDirectory()) {
+                File[] files = sourceDir.listFiles();
+                for (File f : files) {
+                    if (f.isFile()) {
+                        System.out.println("Копируем: " + f.getPath() + " - > " + path(f.getPath()).toPath());
+                        Files.copy(f.toPath(), path(f.getPath()).toPath());
+                    } else {
+                        copyFolder(f.getPath(), path(f.getPath()).getPath());
+                    }
+                }
+            }
+        }
+    }
+
+    private static File path(String s) {
+        String filePath = s.replace(defSouPath, defDestPath);
+        File f = new File(filePath);
+        return f;
     }
 }
--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,10 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String source = "C:\\Users\\antonturov\\Desktop\\java_test";
+        String target = "C:\\Users\\antonturov\\Desktop\\java_test_2";
+
+        FileUtils.copyFolder(source, target);
     }
 }
