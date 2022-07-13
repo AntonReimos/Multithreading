ef188fbf716ad20923bb5664985a3c66be4036a7
antonturov <antonturov@mail.ru>
2022-07-13 09:16:20 +0300

    Json save

--- FilesAndNetwork/ParseMetro/src/main/java/Main.java
+++ FilesAndNetwork/ParseMetro/src/main/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Metro metro = new Metro();
         metro.metro("https://skillbox-java.github.io/",
-                "FilesAndNetwork\\ParseMetro\\src\\main\\resources\\MOSmap.json");
+                "src/main/resources/MOSmap.json");
     }
 }
